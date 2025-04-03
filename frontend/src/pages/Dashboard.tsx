e284eaa910b0d326712e5d63dff8c6e9ce47ad4a
FrostNova <83959988+frostnova721@users.noreply.github.com>
2025-04-03 20:49:01 +0530

    Update Dashboard.tsx

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 export const Dashboard = () => {
   const [showPopup, setPopup] = useState(false);
   const [user, setUser] = useState({ name: "Anonymous", id: "N/A" });
-  const [booked, setBooked] = useState(0);
+  // const [booked, setBooked] = useState(0);
 
   const nav = useNavigate();
 
